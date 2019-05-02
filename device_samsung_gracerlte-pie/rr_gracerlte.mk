9e564c573cc13f851f1c38ce6e3c17ecc8c4f05a
kongacute <thaibinh262@gmail.com>
2019-05-02 18:02:43 +0700

    Stock

---
+++ device_samsung_gracerlte-pie/rr_gracerlte.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
+# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/gracerlte/device_gracerlte.mk)
+$(call inherit-product, device/samsung/gracer-common/gracer-common.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common Resurrection Remix stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := rr_gracerlte
+PRODUCT_DEVICE := gracerlte
+PRODUCT_MODEL := SM-N935F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=gracerltexx \
+    PRIVATE_BUILD_DESC="gracerltexx-user 9 PPR1 N935FXXU4CSC4 release-keys"
+
+BUILD_FINGERPRINT := samsung/gracerltexx/gracerlte:9/PPR1/N935FXXU4CSC4:user/release-keys
---
+++ device_samsung_gracerlte-pie/vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo rr_gracerlte-eng
+add_lunch_combo rr_gracerlte-userdebug
+add_lunch_combo rr_gracerlte-user
